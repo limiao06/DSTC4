0f80860fc2c4d86998ff57467339e9c18f6c001c
limiao06 <limiaogg@126.com>
2015-07-24 22:24:59 +0800

    ensemble tracker

--- scripts/dstc_thu/msiip_ensemble_tracker.py
+++ scripts/dstc_thu/msiip_ensemble_tracker.py
@@ -7,114 +7,99 @@ Combine multiple trackers output a result
 '''
 
 
-import argparse, sys, time, json, os, copy
+import argparse, sys, time, json, os, copy, codecs
 
 import logging
 from GlobalConfig import *
 
-from msiip_nsvc_tracker import msiip_nsvc_tracker
-from association_rule_tracker import association_rule_tracker
 
-sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
-import ontology_reader, dataset_walker
+from BeliefState import BeliefState
+#from msiip_nsvc_tracker import msiip_nsvc_tracker
+#from association_rule_tracker import association_rule_tracker
+
+#sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+#import ontology_reader, dataset_walker
 
 
 '''
-两个重要的问题：
-1. 如何加载这么多模型，配置好参数 ———— 目前的大难点
-2. 如何融合这么多结果
+采用后融合的方式，以其他输出的结果作为输入
 '''
 
-class msiip_nsvc_tracker(object):
+class msiip_ensemble_tracker(object):
 	MY_ID = 'msiip_ensemble'
-	def __init__(self, tagsets, model_dir, ratio_thres = 0.8, max_num = 2, slot_prob_thres = 0.6, value_prob_thres = 0.8, mode = 'hr', bs_mode = 'enhance', bs_alpha = 0.0):
-		self.tagsets = tagsets
-		self.frame = {}
-		self.memory = {}
-		self.beliefstate = BeliefState(bs_mode, bs_alpha)
+	def __init__(self, tagsets, base_dir, config_file, slot_prob_threshold, value_prob_threshold):
+		'''
+		config_file has many lines
+		each line indicates a tracker out json file and its performance
+		'''
+		input = file(config_file)
+		lines = input.readlines()
+		input.close()
 
-		self.slot_prob_threshold = slot_prob_thres
-		self.value_prob_threshold = value_prob_thres
-		self.ratio_thres = ratio_thres
-		self.mode = mode
-
-		self.svc = slot_value_classifier()
-		self.svc.LoadModel(model_dir)
-
-		self.tuple_extractor = Tuple_Extractor()
-		self.rules = DSTC4_rules(tagsets)
+		self.config = GetConfig()
 		self.appLogger = logging.getLogger(self.MY_ID)
+		self.rules = DSTC4_rules(tagsets)
+		self.slot_prob_threshold = slot_prob_threshold
+		self.value_prob_threshold = value_prob_threshold
 
-		if not self.svc.is_set:
-			self.appLogger.error('Error: Fail to load slot_value_classifier model!')
-			raise Exception('Error: Fail to load slot_value_classifier model!')
-		self.value_extractor = value_extractor(tagsets, ratio_thres, max_num)
+		self.logs = []
+		self.weight_list = []
 
+		for line in lines:
+			l = line.strip()
+			if not l:
+				break
+			tokens = l.split('\t')
+			assert len(tokens) == 2
+			file_name = os.path.join(base_dir, tokens[0])
+			log_input = codecs.open(filename, 'r', 'utf-8')
+			self.logs.append(json.load(log_input))
+			log_input.close()
+			weight = float(tokens[1])
+			self.weight_list.append(weight)
 
-	def addUtter(self, utter):
-		self.appLogger.debug('utter_index: %d' % (utter['utter_index']))
-		output = {'utter_index': utter['utter_index']}
-		topic = utter['segment_info']['topic']
-		if topic in self.tagsets:
-			self._UpdateFrameProb(utter)
-			self._UpdateFrame()
-			self.frame = self.rules.prune_frame_label(topic, self.frame)
-			output['frame_prob'] = copy.deepcopy(self.beliefstate.state)
-			output['frame_label'] = copy.deepcopy(self.frame)
-		return output
+			self.appLogger.info('%s\t%.3f' %(filename, weight))
 
-	def _UpdateFrameProb(self, utter):
-		topic = utter['segment_info']['topic']
-		if utter['segment_info']['target_bio'] == 'B':
-			self.frame = {}
-			self.beliefstate.reset()
-			
-		if topic in self.tagsets:
-			transcript = utter['transcript']
-			svc_result, result_prob = self.svc.PredictUtter(utter, self.svc.feature.feature_list)
-			tuples = []
-			probs = []
-			for key in svc_result:
-				label = svc_result[key]
-				prob = result_prob[key][1]
-				if label == 1:
-					tuples.append(key)
-					probs.append(prob)
+		for i in range(len(logs)-1):
+			if logs[i]['dataset'] != logs[i+1]['dataset']:
+				raise Exception('Dataset of input logs mismatch!')
 
-			self.appLogger.debug('transcript: %s' %(transcript))
-			self.appLogger.debug('tuples: %s, probs: %s' %(tuples.__str__(), probs.__str__()))
+		self.dataset = logs[0]['dataset']
+		self.appLogger.info('dataset: %s' %(self.dataset))
+		self.frame_prob = {}
+		self.frame = {}
 
-			# get the prob_frame_labels for the single utterance
-			prob_frame_labels = self.tuple_extractor.generate_frame(tuples, probs, self.mode)
-			self._AddExtractValue(topic, transcript, prob_frame_labels)
+	def ensemble(self):
+		out_json = {"sessions":[]}
+		out_json['dataset'] = self.dataset
+		for i, session in enumerate(self.logs[0]['sessions']):
+			this_session = {"session_id":session["session_id"], "utterances":[]}
+			for j, utter in enumerate(session):
+				sys.stderr.write('%d:%d\n'%(session['session_id'], utter['utter_index']))
+				this_utter = {'utter_index': utter['utter_index']}
+				if 'frame_prob' in utter:
+					frame_prob_list = []
+					for log in self.logs:
+						frame_prob_list.append(log['sessions'][i]['utterances'][j]['frame_prob'])
+					self._UpdateFrameProb(frame_prob_list, self.weight_list)
+					self._UpdateFrame()
+					self.frame = self.rules.prune_frame_label(topic, self.frame)
+					this_utter['frame_prob'] = copy.deepcopy(self.beliefstate.state)
+					this_utter['frame_label'] = copy.deepcopy(self.frame)
+				this_session['utterances'].append(this_utter)
+			out_json["sessions"].append(this_session)
+		return out_json
 
-			# update belief state
-			self.beliefstate.AddFrame(prob_frame_labels)
-
-
-	def _AddExtractValue(self, topic, transcript, prob_frame_labels):
-		'''
-		extract values for non-enumerable slots
-		'''
-		for slot in prob_frame_labels:
-			if slot in self.tagsets[topic]:
-				if not self.tuple_extractor.enumerable(slot):
-					value_list = self.value_extractor.ExtractValue(topic, slot, transcript)
-					for value, ratio in value_list:
-						prob_frame_labels[slot]['values'][value] = ratio * 1.0 / 100
-
+	def _UpdateFrameProb(self, frame_prob_list, weight_list):
+		self.frame = BeliefState.StateEnsemble(frame_prob_list, weight_list)
+	
 	def _UpdateFrame(self):
 		self.frame = {}
-		for slot in self.beliefstate.state:
-			if self.tuple_extractor.enumerable(slot):
-				for value, prob in self.beliefstate.state[slot]['values'].items():
+		for slot in self.frame_prob:
+			if self.frame_prob[slot]['prob'] == -1 or self.frame_prob[slot]['prob'] > self.slot_prob_threshold:
+				for value, prob in self.frame_prob[slot]['values'].items():
 					if prob >= self.value_prob_threshold:
 						self._AddSlotValue2Frame(slot,value)
-			else:
-				if self.beliefstate.state[slot]['prob'] > self.slot_prob_threshold:
-					for value, ratio in self.beliefstate.state[slot]['values'].items():
-						if ratio >= self.ratio_thres:
-							self._AddSlotValue2Frame(slot,value)
 
 	def _AddSlotValue2Frame(self,slot,value):
 		if slot not in self.frame:
@@ -126,26 +111,17 @@ class msiip_nsvc_tracker(object):
 
 
 
-	def reset(self):
-		self.frame = {}
-		self.memory = {}
-		self.beliefstate.reset()
 
 
 
 def main(argv):
-	parser = argparse.ArgumentParser(description='Simple hand-crafted dialog state tracker baseline.')
-	parser.add_argument('--dataset', dest='dataset', action='store', metavar='DATASET', required=True, help='The dataset to analyze')
-	parser.add_argument('--dataroot',dest='dataroot',action='store',required=True,metavar='PATH', help='Will look for corpus in <destroot>/<dataset>/...')
-	parser.add_argument('--model_dir',dest='model_dir',action='store',required=True,metavar='PATH', help='model dir')
+	parser = argparse.ArgumentParser(description='MSIIP ensemble tracker.')
+	parser.add_argument('--LogBaseDir',dest='LogBaseDir',action='store',required=True,help='The base directory that contains the log files')
+	parser.add_argument('--config',dest='config',action='store',required=True,help='Config file, indicate log files and weight')
 	parser.add_argument('--trackfile',dest='trackfile',action='store',required=True,metavar='JSON_FILE', help='File to write with tracker output')
 	parser.add_argument('--ontology',dest='ontology',action='store',metavar='JSON_FILE',required=True,help='JSON Ontology file')
-	parser.add_argument('--ratio_thres',dest='ratio_thres',type=float,action='store',default=0.8,help='ration threshold')
 	parser.add_argument('--value_prob',dest='value_prob',type=float,action='store',default=0.8,help='output value prob threshold')
 	parser.add_argument('--slot_prob',dest='slot_prob',type=float,action='store',default=0.6,help='output slot prob threshold')
-	parser.add_argument('--STCMode',dest='STCMode',action='store',default='hr',help='STC mode, high precision or high recall')
-	parser.add_argument('--BSMode',dest='BSMode',action='store',default='enhance',help='Belief State mode: max, average or enhance')
-	parser.add_argument('--BSAlpha',dest='BSAlpha',type=float,action='store',default=0.0,help='Belief State average history alpha')
 	args = parser.parse_args()
 
 	# 读取配置文件
@@ -160,38 +136,11 @@ def main(argv):
     					level = GetLogLevel(log_level_key), 
     					format = '%(asctime)s %(levelname)8s %(lineno)4d %(module)s:%(name)s.%(funcName)s: %(message)s')
 
-
-
-	dataset = dataset_walker.dataset_walker(args.dataset,dataroot=args.dataroot,labels=False)
 	tagsets = ontology_reader.OntologyReader(args.ontology).get_tagsets()
 
-	track_file = open(args.trackfile, "wb")
-	track = {"sessions":[]}
-	track["dataset"]  = args.dataset
-	start_time = time.time()
-
-	tracker = msiip_nsvc_tracker(tagsets, args.model_dir,
-								ratio_thres = args.ratio_thres, 
-								slot_prob_thres = args.slot_prob, 
-								value_prob_thres = args.value_prob, 
-								mode = args.STCMode, 
-								bs_mode = args.BSMode, 
-								bs_alpha = args.BSAlpha)
-	for call in dataset:
-		this_session = {"session_id":call.log["session_id"], "utterances":[]}
-		tracker.reset()
-		for (utter,_) in call:
-			sys.stderr.write('%d:%d\n'%(call.log['session_id'], utter['utter_index']))
-			tracker_result = tracker.addUtter(utter)
-			if tracker_result is not None:
-				this_session["utterances"].append(tracker_result)
-		track["sessions"].append(this_session)
-	end_time = time.time()
-	elapsed_time = end_time - start_time
-	track['wall_time'] = elapsed_time
-
+	tracker = msiip_ensemble_tracker(tagsets, args.LogBaseDir, args.config, args.slot_prob, args.value_prob)
+	track = tracker.ensemble()
 	json.dump(track, track_file, indent=4)
-
 	track_file.close()
 
 if __name__ =="__main__":
