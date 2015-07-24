d3f7a031781669f48b5b0aba2ff5e223b8837dda
limiao06 <limiaogg@126.com>
2015-07-24 10:52:24 +0800

    add ensemble function

--- scripts/dstc_thu/BeliefState.py
+++ scripts/dstc_thu/BeliefState.py
@@ -112,6 +112,73 @@ class BeliefState(object):
 			else:
 				self.appLogger.error('Unknown mode: %s' %(self.mode))
 
+	@staticmethod
+	def StateEnsemble(bs_list, config):
+		'''
+		bs_list is a list, each item is a BeliefState
+		config indicate the ensemble method:
+			if config is a list of weight, then the ensemble method is average
+			elif config is a string 'vote', the ensemble method is vote
+		'''
+		mode = None
+		if isinstance(config, list):
+			if len(bs_list) != len(config):
+				raise Exception('The ensemble method is average, but the bs vector dose not match the weight vector! %d %d' %(len(bs_list), len(config)))
+			sum_weight = sum(config)
+			weight_vector = [i*1.0/sum_weight for i in config]
+			mode = 'average'
+		elif config == 'vote':
+			mode = 'vote'
+		else:
+			raise Exception('Unknown ensemble method: %s' %(config))
+
+		# prepare out_state
+		out_state = {}
+		for bs in bs_list:
+			for slot in bs:
+				if slot not in out_state:
+					out_state[slot] = {'prob': -1, 'values':{}}
+				for value in bs[slot]['values']:
+					if value not in out_state[slot]['values']:
+						out_state[slot]['values'][value] = 0.0
+
+
+		# ensemble
+		for slot in out_state:
+			# calculate slot prob, slot prob is the max value
+			max_prob = -1
+			for bs in bs_list:
+				if slot in bs:
+					if max_prob < bs[slot]['prob']:
+						max_prob = bs[slot]['prob']
+			out_state[slot]['prob'] = max_prob
+
+			# calculate value prob
+			for value in out_state[slot]['values']:
+				if mode == 'average':
+					value_prob_list = []
+					for bs in bs_list:
+						if slot not in bs:
+							value_prob_list.append(0.0)
+						else:
+							if value not in bs[slot]['values']:
+								value_prob_list.append(0.0)
+							else:
+								value_prob_list.append(bs[slot]['values'][value])
+					for weight, prob in zip(weight_vector, value_prob_list):
+						out_state[slot]['values'][value] += weight * prob
+
+				if mode == 'vote':
+					pass
+					# to be completed
+
+		return out_state
+
+
+
+
+
+
 
 
 
