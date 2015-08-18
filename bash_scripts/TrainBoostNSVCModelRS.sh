7b00d4b8ff080d6f8a8368fac16dbb73c0282c58
limiao06 <limiaogg@126.com>
2015-08-18 22:01:16 +0800

    not finish yet

--- bash_scripts/TrainBoostNSVCModelRS.sh
+++ bash_scripts/TrainBoostNSVCModelRS.sh
@@ -1,9 +1,9 @@
 # remove stop words version
-# not finish yet!! 
+echo not finish yet
 # the first model is not trained from sub_segments data, it is re-trained from an existing model's training samples
 # TrainBoostNSVCModelRS.sh old_feature_list iteration_index new_feature_list iteration_nums
 # ./TrainBoostNSVCModelRS.sh uB 3 u 5
 set -u
 set -e
 # train boost model
-python ../scripts/dstc_thu/nsvc_boosting_method.py --dataset dstc4_train --dataroot ../data --subseg ../output/processed_data/sub_segments_data/sub_segments_train.json --old_model ../output/models/NSVC_models/nsvc_${1}_model_RS_boost/${2} ../output/models/NSVC_models/nsvc_newboost_${1}${2}_${3}_model_RS --it ${4} --ReTrain --RemoveSW --feature ${3} 
+# python ../scripts/dstc_thu/nsvc_boosting_method.py --dataset dstc4_train --dataroot ../data --subseg ../output/processed_data/sub_segments_data/sub_segments_train.json --old_model ../output/models/NSVC_models/nsvc_${1}_model_RS_boost/${2} ../output/models/NSVC_models/nsvc_newboost_${1}${2}_${3}_model_RS --it ${4} --ReTrain --RemoveSW --feature ${3} 
