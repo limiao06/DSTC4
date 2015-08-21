8753f2c6ef2a08510b45a7b0ff56c9cbc4e4760a
limiao06 <limiaogg@126.com>
2015-08-21 16:37:22 +0800

    bug fix

--- bash_scripts/test_boosting_params.sh
+++ bash_scripts/test_boosting_params.sh
@@ -4,13 +4,25 @@
 
 set -u
 set -e
-echo "test_boosting_params" ${1} > ../output/msiip_out/msiip_nsvc_out/test_boosting_params/test_boosting_params.txt
+logfile=../output/msiip_out/msiip_nsvc_out/test_boosting_params/test_boosting_params.txt
+outfile_path=../output/msiip_out/msiip_nsvc_out/test_boosting_params
+outmodel_path=../output/models/NSVC_models/nsvc_test_boost
+echo "test_boosting_params" ${1} > ${logfile}
 
-python ../scripts/dstc_thu/nsvc_sub_segments_tools.py --subseg ../output/processed_data/sub_segments_data/sub_segments_train.json --train --ontology ../scripts/config/ontology_dstc4.json --feature ${1} ../output/models/NSVC_models/nsvc_${1}_model
-python ../scripts/dstc_thu/msiip_nsvc_tracker.py --dataset dstc4_dev --dataroot ../data --ontology ../scripts/config/ontology_dstc4.json --model_dir ../output/models/NSVC_models/nsvc_${1}_model/ --trackfile ../output/msiip_out/msiip_nsvc_out/test_boosting_params/msiip_nsvc_${1}_t80_hr.json
-python ../scripts/score.py --dataset dstc4_dev --dataroot ../data/ --trackfile ../output/msiip_out/msiip_nsvc_out/test_boosting_params/msiip_nsvc_${1}_t80_hr.json --scorefile ../output/msiip_out/msiip_nsvc_out/test_boosting_params/msiip_nsvc_${1}_t80_hr.score --ontology ../scripts/config/ontology_dstc4.json
-echo "base" > ../output/msiip_out/msiip_nsvc_out/test_boosting_params/test_boosting_params.txt
-python ../scripts/report.py --scorefile ../output/msiip_out/msiip_nsvc_out/test_boosting_params/msiip_nsvc_${1}_t80_hr.score >> ../output/msiip_out/msiip_nsvc_out/test_boosting_params/test_boosting_params.txt
+python ../scripts/dstc_thu/nsvc_sub_segments_tools.py \
+	--subseg ../output/processed_data/sub_segments_data/sub_segments_train.json \
+	--train --ontology ../scripts/config/ontology_dstc4.json --feature ${1} \
+	../output/models/NSVC_models/nsvc_${1}_model
+python ../scripts/dstc_thu/msiip_nsvc_tracker.py --dataset dstc4_dev --dataroot ../data \
+	--ontology ../scripts/config/ontology_dstc4.json \
+	--model_dir ../output/models/NSVC_models/nsvc_${1}_model/ \
+	--trackfile ${outfile_path}/msiip_nsvc_${1}_t80_hr.json
+python ../scripts/score.py --dataset dstc4_dev --dataroot ../data \
+	--trackfile ${outfile_path}/msiip_nsvc_${1}_t80_hr.json \
+	--scorefile ${outfile_path}/msiip_nsvc_${1}_t80_hr.score \
+	--ontology ../scripts/config/ontology_dstc4.json
+echo "base" > ${logfile}
+python ../scripts/report.py --scorefile ${outfile_path}/msiip_nsvc_${1}_t80_hr.score >> ${logfile}
 
 
 high_thres_vec=(0.6 0.7 0.75 0.8 0.85 0.9)
@@ -20,13 +32,23 @@ for high_thres in ${high_thres_vec[@]}
 do
 	for low_thres in ${low_thres_vec[@]}
 	do
-		python ../scripts/dstc_thu/nsvc_boosting_method.py --dataset dstc4_train --dataroot ../data --subseg ../output/processed_data/sub_segments_data/sub_segments_train.json --old_model ../output/models/NSVC_models/nsvc_${1}_model ../output/models/NSVC_models/nsvc_test_boost/nsvc_boost_${1}_HT${high_thres}_LT{low_thres} --it ${2} --ht ${high_thres} -- lt ${low_thres}
+		python ../scripts/dstc_thu/nsvc_boosting_method.py --dataset dstc4_train --dataroot ../data \
+			--subseg ../output/processed_data/sub_segments_data/sub_segments_train.json \
+			--old_model ../output/models/NSVC_models/nsvc_${1}_model \
+			${outmodel_path}/nsvc_boost_${1}_HT${high_thres}_LT{low_thres} \
+			--it ${2} --ht ${high_thres} --lt ${low_thres}
 		for it in $(seq 0 $[${2}-1])
 		do
-		  python ../scripts/dstc_thu/msiip_nsvc_tracker.py --dataset dstc4_dev --dataroot ../data/ --model_dir ../output/models/NSVC_models/nsvc_test_boost/nsvc_boost_${1}_HT${high_thres}_LT{low_thres}/${it}/ --trackfile ../output/msiip_out/msiip_nsvc_out/test_boosting_params/msiip_nsvc_${1}_boost_HT${high_thres}_LT{low_thres}_${it}_t80_hr.json --ontology ../scripts/config/ontology_dstc4.json
-		  python ../scripts/score.py --dataset dstc4_dev --dataroot ../data/ --trackfile ../output/msiip_out/msiip_nsvc_out/test_boosting_params/msiip_nsvc_${1}_boost_HT${high_thres}_LT{low_thres}_${it}_t80_hr.json --scorefile ../output/msiip_out/msiip_nsvc_out/test_boosting_params/msiip_nsvc_${1}_boost_HT${high_thres}_LT{low_thres}_${it}_t80_hr.score --ontology ../scripts/config/ontology_dstc4.json
-		  echo "high_thres:" $high_thres ", low_thres:" $low_thres ", boost" ${it} >> ../output/msiip_out/msiip_nsvc_out/test_boosting_params/test_boosting_params.txt
-		  python ../scripts/report.py --scorefile ../output/msiip_out/msiip_nsvc_out/test_boosting_params/msiip_nsvc_${1}_boost_HT${high_thres}_LT{low_thres}_${it}_t80_hr.score >> ../output/msiip_out/msiip_nsvc_out/test_boosting_params/test_boosting_params.txt
+			python ../scripts/dstc_thu/msiip_nsvc_tracker.py --dataset dstc4_dev --dataroot ../data/ \
+				--model_dir ${outmodel_path}/nsvc_boost_${1}_HT${high_thres}_LT{low_thres}/${it}/ \
+				--trackfile ${outfile_path}/msiip_nsvc_${1}_boost_HT${high_thres}_LT{low_thres}_${it}_t80_hr.json \
+				--ontology ../scripts/config/ontology_dstc4.json
+			python ../scripts/score.py --dataset dstc4_dev --dataroot ../data/ \
+				--trackfile ${outfile_path}/msiip_nsvc_${1}_boost_HT${high_thres}_LT{low_thres}_${it}_t80_hr.json \
+				--scorefile ${outfile_path}/msiip_nsvc_${1}_boost_HT${high_thres}_LT{low_thres}_${it}_t80_hr.score \
+				--ontology ../scripts/config/ontology_dstc4.json
+			echo "high_thres:" $high_thres ", low_thres:" $low_thres ", boost" ${it} >> ${logfile}
+			python ../scripts/report.py --scorefile ${outfile_path}/msiip_nsvc_${1}_boost_HT${high_thres}_LT{low_thres}_${it}_t80_hr.score >> ${logfile}
 		done
 	done
 done
