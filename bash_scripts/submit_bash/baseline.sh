0056b134cce9d224c38b64e664d539746d34a09b
limiao06 <limiaogg@126.com>
2015-08-27 09:25:59 +0800

    add submit bash

--- bash_scripts/submit_bash/baseline.sh
+++ bash_scripts/submit_bash/baseline.sh
@@ -1,2 +1,24 @@
 # baseline
+# baseline test or baseline dev
+base_path=../..
+if [ $# -ne 2 ];then
+	echo ${0} "test | dev"
+	exit
+fi
 
+if [ ${1} == dev ];then
+	python ${base_path}/scripts/baseline.py --dataset dstc4_dev --dataroot ${base_path}/data \
+		--trackfile ${base_path}/submit/baseline/answer_dev.json \
+		--ontology ${base_path}/scripts/config/ontology_dstc4.json
+	python ${base_path}/scripts/score.py --dataset dstc4_dev --dataroot ${base_path}/data \
+		--trackfile ${base_path}/submit/baseline/answer_dev.json \
+		--scorefile ${base_path}/submit/baseline/answer_dev.score \
+		--ontology ${base_path}/scripts/config/ontology_dstc4.json
+	python ${base_path}/scripts/report.py --scorefile ${base_path}/submit/baseline/answer_dev.score
+fi
+
+if [ ${1} == "test" ];then
+	python ${base_path}/scripts/baseline.py --dataset dstc4_test --dataroot ${base_path}/data \
+		--trackfile ${base_path}/submit/baseline/answer_test.json \
+		--ontology ${base_path}/scripts/config/ontology_dstc4.json
+fi
