ff3bbfd2754abc9b940de6df696380828c382ebc
limiao <limiaogg@126.com>
2015-08-27 15:12:20 +0800

    add ass_rule submit shell

---
+++ bash_scripts/submit_bash/ass_rule.sh
@@ -0,0 +1,25 @@
+# ass_rule
+# ass_rule test or baseline dev
+base_path=../..
+if [ $# -ne 1 ];then
+	echo ${0} "test | dev"
+	exit
+fi
+
+if [ ${1} == dev ];then
+	python ${base_path}/dstc_thu/association_rule_tracker.py --dataset dstc4_dev --dataroot ${base_path}/data \
+		
+    --trackfile ${base_path}/submit/baseline/answer_dev.json \
+		--ontology ${base_path}/scripts/config/ontology_dstc4.json
+	python ${base_path}/scripts/score.py --dataset dstc4_dev --dataroot ${base_path}/data \
+		--trackfile ${base_path}/submit/baseline/answer_dev.json \
+		--scorefile ${base_path}/submit/baseline/answer_dev.score \
+		--ontology ${base_path}/scripts/config/ontology_dstc4.json
+	python ${base_path}/scripts/report.py --scorefile ${base_path}/submit/baseline/answer_dev.score
+fi
+
+if [ ${1} == "test" ];then
+	python ${base_path}/scripts/baseline.py --dataset dstc4_test --dataroot ${base_path}/data \
+		--trackfile ${base_path}/submit/baseline/answer_test.json \
+		--ontology ${base_path}/scripts/config/ontology_dstc4.json
+fi
