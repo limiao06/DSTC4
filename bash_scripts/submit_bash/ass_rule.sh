b9e9443ad7f247050ed4527e492e3dc1078c9f19
limiao06 <limiaogg@126.com>
2015-08-27 15:18:04 +0800

    add ass_rule submit bash

--- bash_scripts/submit_bash/ass_rule.sh
+++ bash_scripts/submit_bash/ass_rule.sh
@@ -8,18 +8,23 @@ fi
 
 if [ ${1} == dev ];then
 	python ${base_path}/dstc_thu/association_rule_tracker.py --dataset dstc4_dev --dataroot ${base_path}/data \
-		
-    --trackfile ${base_path}/submit/baseline/answer_dev.json \
+		--ar ${base_path}/output/models/association_rule_models/association_train_rule.json
+		--stm ${base_path}/output/models/SemTagModel/semtag_train_model
+		--trackfile ${base_path}/submit/ass_rule/answer_dev.json \
 		--ontology ${base_path}/scripts/config/ontology_dstc4.json
+		--pt 0.8
 	python ${base_path}/scripts/score.py --dataset dstc4_dev --dataroot ${base_path}/data \
-		--trackfile ${base_path}/submit/baseline/answer_dev.json \
-		--scorefile ${base_path}/submit/baseline/answer_dev.score \
+		--trackfile ${base_path}/submit/ass_rule/answer_dev.json \
+		--scorefile ${base_path}/submit/ass_rule/answer_dev.score \
 		--ontology ${base_path}/scripts/config/ontology_dstc4.json
-	python ${base_path}/scripts/report.py --scorefile ${base_path}/submit/baseline/answer_dev.score
+	python ${base_path}/scripts/report.py --scorefile ${base_path}/submit/ass_rule/answer_dev.score
 fi
 
 if [ ${1} == "test" ];then
-	python ${base_path}/scripts/baseline.py --dataset dstc4_test --dataroot ${base_path}/data \
-		--trackfile ${base_path}/submit/baseline/answer_test.json \
+	python ${base_path}/dstc_thu/association_rule_tracker.py --dataset dstc4_test --dataroot ${base_path}/data \
+		--ar ${base_path}/output/models/association_rule_models/association_train_rule.json
+		--stm ${base_path}/output/models/SemTagModel/semtag_train_model
+		--trackfile ${base_path}/submit/ass_rule/answer_test.json \
 		--ontology ${base_path}/scripts/config/ontology_dstc4.json
+		--pt 0.8
 fi
