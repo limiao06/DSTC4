37cdf3b68b27013d9c5241775936267959849ef6
limiao <limiaogg@126.com>
2015-08-28 08:32:40 +0800

    little change

---
+++ bash_scripts/submit_bash/baseline_low.sh
@@ -0,0 +1,24 @@
+# baseline
+# baseline test or baseline dev
+base_path=../..
+if [ $# -ne 1 ];then
+	echo ${0} "test | dev"
+	exit
+fi
+
+if [ ${1} == dev ];then
+	python ${base_path}/scripts/baseline_low.py --dataset dstc4_dev --dataroot ${base_path}/data \
+		--trackfile ${base_path}/submit/baseline/low_answer_dev.json \
+		--ontology ${base_path}/scripts/config/ontology_dstc4.json
+	python ${base_path}/scripts/score.py --dataset dstc4_dev --dataroot ${base_path}/data \
+		--trackfile ${base_path}/submit/baseline/low_answer_dev.json \
+		--scorefile ${base_path}/submit/baseline/low_answer_dev.score \
+		--ontology ${base_path}/scripts/config/ontology_dstc4.json
+	python ${base_path}/scripts/report.py --scorefile ${base_path}/submit/baseline/low_answer_dev.score
+fi
+
+if [ ${1} == "test" ];then
+	python ${base_path}/scripts/baseline_low.py --dataset dstc4_test --dataroot ${base_path}/data \
+		--trackfile ${base_path}/submit/baseline/low_answer_test.json \
+		--ontology ${base_path}/scripts/config/ontology_dstc4.json
+fi
