069b7a7c6c5962cebfbdb4ce045b4149d0a09e91
limiao <limiaogg@126.com>
2015-08-31 21:01:55 +0800

    add 2 models in nsvc

--- bash_scripts/submit_bash/nsvc.sh
+++ bash_scripts/submit_bash/nsvc.sh
@@ -1,27 +1,27 @@
 # nsvc_u
 # nsvc_u test or baseline dev
 base_path=../..
-if [ $# -ne 2 ];then
-	echo ${0} "u|uB test|dev"
+if [ $# -ne 3 ];then
+	echo ${0} "u|uB 1 test|dev"
 	exit
 fi
 
-if [ ${2} == dev ];then
+if [ ${3} == dev ];then
 	python ${base_path}/scripts/dstc_thu/msiip_nsvc_tracker.py --dataset dstc4_dev --dataroot ${base_path}/data \
 		--ontology ${base_path}/scripts/config/ontology_dstc4.json \
-		--model_dir ${base_path}/submit/nsvc/Feat_${1}/model/ \
+		--model_dir ${base_path}/submit/nsvc/Feat_${1}/model${2}/ \
     --slot_prob 0.5 \
-		--trackfile ${base_path}/submit/nsvc/Feat_${1}/answer_dev.json
+		--trackfile ${base_path}/submit/nsvc/Feat_${1}/answer_${2}_dev.json
 	python ${base_path}/scripts/score.py --dataset dstc4_dev --dataroot ${base_path}/data \
-		--trackfile ${base_path}/submit/nsvc/Feat_${1}/answer_dev.json \
-		--scorefile ${base_path}/submit/nsvc/Feat_${1}/answer_dev.score \
+		--trackfile ${base_path}/submit/nsvc/Feat_${1}/answer_${2}_dev.json \
+		--scorefile ${base_path}/submit/nsvc/Feat_${1}/answer_${2}_dev.score \
 		--ontology ${base_path}/scripts/config/ontology_dstc4.json
-	python ${base_path}/scripts/report.py --scorefile ${base_path}/submit/nsvc/Feat_${1}/answer_dev.score
+	python ${base_path}/scripts/report.py --scorefile ${base_path}/submit/nsvc/Feat_${1}/answer_${2}_dev.score
 fi
 
-if [ ${2} == "test" ];then
+if [ ${3} == "test" ];then
 	python ${base_path}/scripts/dstc_thu/msiip_nsvc_tracker.py --dataset dstc4_test --dataroot ${base_path}/data \
 		--ontology ${base_path}/scripts/config/ontology_dstc4.json \
-		--model_dir ${base_path}/submit/nsvc/Feat_${1}/model/ \
-		--trackfile ${base_path}/submit/nsvc/Feat_${1}/answer_test.json
+		--model_dir ${base_path}/submit/nsvc/Feat_${1}/model${2}/ \
+		--trackfile ${base_path}/submit/nsvc/Feat_${1}/answer_${2}_test.json
 fi
